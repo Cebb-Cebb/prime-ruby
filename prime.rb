a90cbf49363a0c8024ec7807dd57429a0ddacb20
user <github email address>
2020-10-29 20:45:34 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,11 +1,8 @@
 require 'pry'
 
 def prime?(num)
-   
-# (2..-1)to_a{|i| num % i == 0} 
-
- (2..-1).any?{|i| num % i == 0}
-
+  (2..-1).any?{|i| num % i == 0} # passes false
+ #(2..-1).none?{|i| num % i == 0} # passes true 
 end
 
    
